a38459ad632f8635222ced9824ce6357bfc215c0
Jane <JaneBethPerez@gmail.com>
2020-02-17 13:41:24 -0600

    2,3 to do

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,7 +14,24 @@ struct ContentView: View {
     
     let tipPercentages = [10,15,20,25,0]
     
+    //let doubleValue = Double(stringValue)
+    //let stringValue = "0.5"
+    
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+        
+    }
+    
     var body: some View {
+        NavigationView{
         Form {
             Section {
                 TextField("Amount", text: $checkAmount)
@@ -25,11 +42,24 @@ struct ContentView: View {
                     }
                 }
             }
+            
+            Section(header: Text("How much tip do you want to leave?")){
+                Picker("Tip percentage", selection: $tipPercentage){
+                    ForEach(0 ..< tipPercentages.count){
+                        Text("\(self.tipPercentages[$0])%")
+                    }
+                    } .pickerStyle(SegmentedPickerStyle())
+            }
 
-            Section {
-                Text("$\(checkAmount)")
+            Section(header: Text("Amount per person")){
+                Text("$\(totalPerPerson, specifier: "%.2f")")
+            }
+            Section(header: Text("Total Amount")){
+                Text("$\(grandTotal, specifier: "%.2f")")
             }
         }
+        .navigationBarTitle("WeSplit")
+    }
     }
 }
 
